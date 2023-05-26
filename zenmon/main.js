7dd428a649931d3f46d7fc81b5aa14611d0a2296
Gabriel TOUZALIN <GTO86@MacBook-Air-de-Gabriel.local>
2023-05-26 13:31:51 +0200

    code cleaning + commenting functions

--- zenmon/main.js
+++ zenmon/main.js
@@ -4,40 +4,42 @@ const app  = express();
 
 app.use(express.json());
 
+// API INIT //
+
 app.listen(
 	8082,
-	() => console.log('cest ici http://localhost:8081')
+	() => console.log('cest ici http://localhost:8082')
 );
 
+// TEST DATA //
 const tests = [
 	{id:1, name: 'nays', mail: 'mathys.vitiell@gmail.com', happiness: 5, animal: 'cat'},
 	{id:2, name: 'gigi', mail: 'gigi@outlook.fr', happiness: 4, animal: 'chien'},
 ];
-const thename = [
-];
-let searchname = "";
 
 
-
+// RETRIEVE ALL SURVEY ENTRIES //
 
 app.get('/api/test', (req, res) => {
 	res.send(tests);
 });
-app.get('/api/nom', (req, res) => {
-	const termeknevFlat = thename.flat().map(({ name }) => name);
-	res.send(thename);
-});
+
+
+let searchname = "";
+
+// RETRIEVE FAVORITE ANIMAL FROM USER //
 
 app.post('/api/nom', (req, res) => {
-
 	searchname = req.body.name;
 	const perso = tests.find(c => c.name == searchname);
 	if (!perso) res.status(404).send('Ne trouve pas name');
 	if (!perso) console.log ("Pas de resultat pour : " + searchname);
 	res.send(perso);
 	if (perso) console.log("L'animal preferee de " + searchname + " est : " + perso.animal);
-
 });
+
+
+// UPDATE SURVEY ENTRIES //
 
 app.post('/api/test', (req, res) => {
 	const test = {
@@ -51,11 +53,11 @@ app.post('/api/test', (req, res) => {
 	res.send(test);
 });
 
+// SHOW USER ENTRIES //
 
 app.get('/api/test/:name', (req, res) => {
 	const perso = tests.find(c => c.name == req.params.name);
 	if (!perso) res.status(404).send('Ne trouve pas name');
 	res.send(perso);
-
 	//afficher animal 
 });
